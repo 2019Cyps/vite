9d87a2af37ff5e19482f1ea31ed64ae105ea5437
chengyupeng <15735938646@163.com>
2023-06-19 10:09:32 +0800

    init

--- src/views/layout/components/slidebar.vue
+++ src/views/layout/components/slidebar.vue
@@ -1,19 +1,18 @@
 <template>
     <el-menu>
-        <el-sub-menu index="1">
+        <el-sub-menu v-for="(item,index) in menuListObj" :index="index">
             <template #title>
-                <span>菜单1</span>
+                <span>{{item.name}}</span>
             </template>
-            <el-menu-item-group title="Group One">
-                <el-menu-item index="1-1">item one</el-menu-item>
-                <el-menu-item index="1-2">item two</el-menu-item>
+            <el-menu-item-group v-if="item.children">
+                <el-menu-item index='1-1' v-for="(i,index) in item.children">{{i.name}}</el-menu-item>
             </el-menu-item-group>
             <el-sub-menu index="1-3">
                 <template #title>item four</template>
                 <el-menu-item index="1-2-1">？？？</el-menu-item>
             </el-sub-menu>
         </el-sub-menu>
-        <el-sub-menu index="2">
+        <!-- <el-sub-menu index="2">
             <template #title>
                 <span>菜单2</span>
             </template>
@@ -22,13 +21,14 @@
             <template #title>
                 <span>菜单3</span>
             </template>
-        </el-sub-menu>
+        </el-sub-menu> -->
     </el-menu>
 </template>
 
 <script setup>
 import { menuList } from "../../../utils/api";
+let menuListObj = ref('')
 menuList().then(res => {
-    
+    menuListObj.value = res.data
 })
 </script>
